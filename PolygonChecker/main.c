d87ea6d4f67cdefc3c7e1b17e5a6f1e0537d8775
Curtis <113472878+CurtisConestoga@users.noreply.github.com>
2022-11-16 13:14:02 -0500

    Added TriangleInsideAngle function - did not run so commented out

--- PolygonChecker/main.c
+++ PolygonChecker/main.c
@@ -8,6 +8,7 @@
 int side = 0;
 
 int main() {
+	printf("Hello");
 	bool continueProgram = true;
 	while (continueProgram) {
 		printWelcome();
@@ -36,10 +37,12 @@ int main() {
 		case 1:
 			printf_s("Triangle selected.\n");
 			int triangleSides[3] = { 0, 0, 0 };
+			//float triangleAngles[3] = { 0, 0, 0 };
 			int* triangleSidesPtr = getTriangleSides(triangleSides);
 			//printf_s("! %d\n", triangleSidesPtr[0]);
 			char* result = analyzeTriangle(triangleSidesPtr[0], triangleSidesPtr[1], triangleSidesPtr[2]);
 			printf_s("%s\n", result);
+			//triangleInsideAngle(triangleSidesPtr, triangleAngles);
 			break;
 		case 0:
 			continueProgram = false;
